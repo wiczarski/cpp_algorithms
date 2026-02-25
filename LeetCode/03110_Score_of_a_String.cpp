23106a5b3aab312f67b4bb4eeecb53416ee7141f
wiczarski <12345678+wiczarski@users.noreply.github.com>
2026-02-25 09:59:48 +0100

    fix(LeetCode): resend code of task 03110

---
+++ LeetCode/03110_Score_of_a_String.cpp
@@ -0,0 +1,14 @@
+#include <bites/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            sum += abs(s[i] - s[i + 1]);
+        }
+        return sum;
+    }
+};
