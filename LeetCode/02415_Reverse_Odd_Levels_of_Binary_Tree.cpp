b239e42be5536f0e4187dc20de5a2023e0c9bfcb
wiczarski <12345678+wiczarski@users.noreply.github.com>
2026-03-03 09:05:43 +0100

    feat(LeetCode): add task 02415

---
+++ LeetCode/02415_Reverse_Odd_Levels_of_Binary_Tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+TreeNode* cloneNode(TreeNode* node) {
+    if (!node) return nullptr;
+
+    return new TreeNode(
+        node->val,
+        cloneNode(node->left),
+        cloneNode(node->right)
+    );
+}
+
+void mirrorOdds(TreeNode* a, TreeNode* b, int lvl) {
+    if (a->left == nullptr) return;
+    if(lvl % 2 == 0) {
+        b->right->val = a->left->val;
+        b->left->val = a->right->val;
+    }
+    lvl++;
+    mirrorOdds(a->left, b->right, lvl);
+    mirrorOdds(a->right, b->left, lvl);
+}
+
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        TreeNode* mirror_root = cloneNode(root);
+        mirrorOdds(root, mirror_root, 0);
+        return mirror_root;
+    }
+};
