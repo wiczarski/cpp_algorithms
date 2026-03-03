db8161339e92307fd6fa427c8f6306c40d81fb98
wiczarski <12345678+wiczarski@users.noreply.github.com>
2026-03-03 09:44:57 +0100

    feat(LeetCode): add task 00703

---
+++ LeetCode/00703_Kth_Largest_Element_in_a_Stream.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+public:
+    KthLargest(int k, vector<int>& nums) : k(k){
+        for(int x : nums) add(x);
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > k) pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
