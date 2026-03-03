6cef09db86e49509a76f8c5187239e43f0fc6f5e
wiczarski <12345678+wiczarski@users.noreply.github.com>
2026-03-03 12:48:16 +0100

    feat(LeetCode): add task 01046

---
+++ LeetCode/01046_Last_Stone_Weight.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>, less<int>> pq(stones.begin(), stones.end());
+        while(pq.size() > 1) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if (a != b) {
+                pq.push(max(a, b) - min(a, b));
+            }
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
