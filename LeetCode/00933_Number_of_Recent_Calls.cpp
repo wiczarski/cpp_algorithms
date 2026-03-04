41edfe1e0e10abf8b7ec4a75dc0875d0be8b6dfa
wiczarski <12345678+wiczarski@users.noreply.github.com>
2026-03-04 14:03:26 +0100

    feat(LeetCode): add task 00933

---
+++ LeetCode/00933_Number_of_Recent_Calls.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class RecentCounter {
+    queue<int> q;
+
+public:
+    RecentCounter() {}
+    
+    int ping(int t) {
+        q.push(t);
+        while(!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
